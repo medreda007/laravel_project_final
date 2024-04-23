af78aa019288c85f1279b13e933b9d371ed2e4e0
medreda007 <karimmedreda@gmail.com>
2024-04-23 22:51:33 +0100

    menu

--- resources/views/Checkin/checkin.blade.php
+++ resources/views/Checkin/checkin.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.index')
 
 @section('main')
-    <div class="">
-        
+    <div class=" flex flex-col">
+        {{ auth()->user()->name }}
     </div>
+    @php
+        // Get the menus associated with the authenticated user
+        $userMenus = auth()->user()->menus;
+    @endphp
+
+    @foreach ($userMenus as $menu)
+        {{ $menu->name }}
+    @endforeach
 @endsection
